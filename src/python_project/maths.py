7467f3c09f3012a9e75676405c4340fd26f54e0a
Redmar <redmar@ubuntu.com>
2021-07-09 08:44:02 +0200

    Add support for doctests

--- src/python_project/maths.py
+++ src/python_project/maths.py
@@ -1,2 +1,13 @@
 def addition(a, b):
+    """ Add two numbers together
+
+    >>> addition(1, 1)
+    2
+    >>> addition(0, 0)
+    0
+    >>> addition(-1, 1)
+    0
+    >>> addition(-1, -1)
+    -2
+    """
     return a + b
